789128eea90cb807a724b0685a8bbc636560193d
Debdipbanerjee <debdipbanerjee7@gmail.com>
2020-12-16 02:35:21 +0530

    Click to move implemented

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,16 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] Transform target;
-
-
     // Update is called once per frame
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        //casting ray from camera to a point
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit); //putting information of ray into hit
+
+        //if ray hits somewhere, player moves to that point, otherwise not
+        if(hasHit == true)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+
     }
 }
