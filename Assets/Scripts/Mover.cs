359622e61e3695f7ac6cb041758cd523d5450bbc
Debdipbanerjee <debdipbanerjee7@gmail.com>
2020-12-16 03:00:11 +0530

    Follow Camera Implemented

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -9,7 +9,7 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButton(0))
         {
             MoveToCursor();
         }
