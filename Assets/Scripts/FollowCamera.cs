e64645f74628af2c879848b2e19a9e5386ec95a5
Debdipbanerjee <debdipbanerjee7@gmail.com>
2020-12-17 17:28:09 +0530

    Refined Movement

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -7,7 +7,7 @@ public class FollowCamera : MonoBehaviour
     [SerializeField] Transform target;
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = target.position;
     }
