80d0b6101d2363452416300f3295f873cc56ded6
Debdipbanerjee <debdipbanerjee7@gmail.com>
2020-12-19 22:22:30 +0530

    Throttling player attack added

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -8,10 +8,15 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour , IAction 
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+
         Transform target;
+        float timeSinceLastAttack = 0;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             //if there's no enemy, don't fight
             if (target == null) return;
 
@@ -22,8 +27,18 @@ namespace RPG.Combat
             }
             else
             {
-                //There's an enemy but in your range, don't move & attack
+                //There's an enemy but in your range, Stop moving, start attacking
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            if(timeSinceLastAttack > timeBetweenAttacks)
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
             }
         }
 
@@ -41,6 +56,12 @@ namespace RPG.Combat
         public void Cancel()
         {
             target = null;
+        }
+
+        //Animation event
+        void Hit()
+        {
+
         }
     }
 }
