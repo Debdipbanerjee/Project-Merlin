7b7479ae44cdfd42b4446be0eea999c39d6e6d8f
Debdipbanerjee <debdipbanerjee7@gmail.com>
2020-12-21 22:25:33 +0530

    Calculated AI chase distance

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -5,7 +5,20 @@ namespace RPG.Control
 {
     public class AIController : MonoBehaviour
     {
+        [SerializeField] float chaseDistance = 5f;
 
-        
+        private void Update()
+        {
+            if (DistanceToPlayer() < chaseDistance)
+            {
+                print(gameObject.name + " will chase that motherfucker");
+            }
+        }
+
+        private float DistanceToPlayer()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            return Vector3.Distance(player.transform.position, transform.position);
+        }
     }
 }
