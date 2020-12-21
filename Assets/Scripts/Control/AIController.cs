55450092490b861e6a4bc1e6ed4a483b06977a7a
Debdipbanerjee <debdipbanerjee7@gmail.com>
2020-12-22 02:13:26 +0530

    Made custom gizmos to visualise chase radius

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -41,5 +41,12 @@ namespace RPG.Control
             float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
             return distanceToPlayer < chaseDistance;
         }
+
+        //Called by Unity
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        }
     }
 }
