e8ab688e0bf18b27eb7ecda6571ee09240e67eee
Debdipbanerjee <debdipbanerjee7@gmail.com>
2020-12-23 01:58:09 +0530

    Waypoint with gizmos created

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        //radius of waypoint gizmos
+        const float waypointGizmoRadius = 0.3f;
+
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmoRadius);
+            }
+        }
+    }
+}
